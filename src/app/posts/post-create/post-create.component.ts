37db90301733e2fdd8656e65a561963148380f45
Aidas Pupelis <aidas.pupelis@asseco.lt>
2020-02-17 17:39:32 +0200

    node + express

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,8 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PostsService } from '../posts.service';
+
+// import {Post} from '../post.model';
 
 @Component({
     selector: 'app-post-create',
@@ -8,14 +12,21 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class PostCreateComponent{
     enteredTitle = '';
     enteredContent = '';
-    @Output()
-    postCreatedEvent = new EventEmitter();
+    // @Output() postCreatedEvent = new EventEmitter<Post>();
+    postCreatedEvent = new EventEmitter<Post>();
 
-    onAddPost() {
-        const post = {
-            title: this.enteredTitle,
-            content: this.enteredContent
-        };
-        this.postCreatedEvent.emit(post);
+    constructor(public postService: PostsService) { }
+
+    onAddPost(form: NgForm) {
+        if (form.invalid) {
+            return;
+        }
+        // const post: Post = {
+        //     title: form.value.title,
+        //     content: form.value.content
+        // };
+        // this.postCreatedEvent.emit(post);
+        this.postService.addPost(form.value.title, form.value.content);
+        form.reset();
     }
 }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,24 +1,36 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+// import {Post} from '../post.model';
+import { PostsService } from './../posts.service';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent implements OnInit {
-
-  constructor() { }
+export class PostListComponent implements OnInit, OnDestroy {
 
   // posts = [
   //   { title: 'Firt Post', content: 'This is the first post content!'},
   //   { title: '2 Post', content: 'This is the 2 post content!'},
   //   { title: '3 Post', content: 'This is the 3 post content!'},
   // ];
+  // @Input() posts: Post[] = [];
+  posts: Post[] = [];
+  private postSub: Subscription;
 
-  @Input()
-  posts = [];
+  constructor(public postsService: PostsService) { }
 
   ngOnInit(): void {
+    // this.posts = this.postsService.getPosts();
+    // this.postSub = this.postsService.getPostUpdateListener()
+    //   .subscribe((posts: Post[]) => {
+    //       this.posts = posts;
+    //   });
+    this.postsService.getPosts();
   }
 
+  ngOnDestroy(): void {
+    this.postSub.unsubscribe();
+  }
 }
